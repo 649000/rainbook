c21becef25f8d062aaca1abb30631e08ef9baf53
nazri610 <nazri610@a7e92145-fe76-fe38-f463-d89284f120cb>
2010-06-11 07:36:05 +0000

    Worked on some stuff on ProfileController. xoxo.

--- Rainbook/grails-app/controllers/app/ProfileController.groovy
+++ Rainbook/grails-app/controllers/app/ProfileController.groovy
@@ -2,5 +2,58 @@ package app
 
 class ProfileController {
 
-    def index = { }
+    def index = {
+        redirect (action: "create")
+    }
+
+    def create =
+    {
+        def profile = new Profile(params)
+        return [profile: profile]
+    }
+
+    def show =
+    {
+        def profile = Profile.get(params.id)
+        return [profile: profile]
+    }
+
+    def save = {
+
+        def profile = new Profile(params)
+                
+        def uploadedFile = request.getFile('photo')
+
+        profile.photo = uploadedFile.originalFilename
+
+        if (profile.save()) { // Save Domain Class
+
+           
+
+            def webRootDir = servletContext.getRealPath("/")
+
+            def userDir = new File(webRootDir, "/photo")
+            userDir.mkdirs()
+
+            // Save file contents into a new file in “/photo” folder.
+            
+            uploadedFile.transferTo(new File(userDir,uploadedFile.originalFilename))
+            
+            redirect(action: "show", id: profile.id)
+        }
+
+        else {
+
+            render(view: "create", model: [profile: profile])
+        }
+    }
+        def delete =
+    {
+        def profile = profile.get(params.id)
+        entry.delete()
+        redirect(action:"create")
+    }
+    
+
 }
+        
