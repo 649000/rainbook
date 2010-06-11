6d88bbd815fcb33070162fc8390dbc37f451ae7a
yuanjie91 <yuanjie91@a7e92145-fe76-fe38-f463-d89284f120cb>
2010-06-11 07:43:21 +0000

    Amended FriendshipController. Able to retrieve the raw list of the user's friends now.

--- trunk/Rainbook/grails-app/controllers/app/FriendshipController.groovy
+++ trunk/Rainbook/grails-app/controllers/app/FriendshipController.groovy
@@ -5,7 +5,16 @@ class FriendshipController
 
     def index =
     {
+        session.userid = 1
+        redirect(action: "list")
+    }
 
+    def list =
+    {
+        def friendList = Friendship.findAllByPartiesLike( session.userid + ",%")
+        def friendList2 = Friendship.findAllByPartiesLike( "%," + session.userid )
+        def completeFriendList = friendList + friendList2
+        render(completeFriendList)
     }
     
 }
