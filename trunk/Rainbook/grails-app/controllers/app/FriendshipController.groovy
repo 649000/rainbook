1b85854fc3c6394a2793aaa25fd25cdd769baf36
yuanjie91 <yuanjie91@a7e92145-fe76-fe38-f463-d89284f120cb>
2010-06-11 08:26:08 +0000

    Amended FriendshipController. Closure for listing of friends done.

--- trunk/Rainbook/grails-app/controllers/app/FriendshipController.groovy
+++ trunk/Rainbook/grails-app/controllers/app/FriendshipController.groovy
@@ -13,8 +13,32 @@ class FriendshipController
     {
         def friendList = Friendship.findAllByPartiesLike( session.userid + ",%")
         def friendList2 = Friendship.findAllByPartiesLike( "%," + session.userid )
-        def completeFriendList = friendList + friendList2
-        render(completeFriendList)
+
+        def completeFriendIDList = new ArrayList();
+        
+        // Retrieve the ID of the other party from friendList
+        for(Friendship f : friendList)
+        {
+            String[] parties = f.toString().split(",")
+            completeFriendIDList.add(parties[1])
+        }
+
+        // Retrieve the ID of the other party from friendList2
+        for(Friendship f : friendList2)
+        {
+            String[] parties = f.toString().split(",")
+            completeFriendIDList.add(parties[0])
+        }
+
+        def completeFriendList = new ArrayList()
+        // For each user ID, retrieve name, email and photo of friend
+        for(String s : completeFriendIDList)
+        {
+            completeFriendList.add( Profile.findById(s) )
+        }
+
+        // def c = friendList + friendList2
+        render(view: "list", model: [friends: completeFriendList] )
     }
     
 }
